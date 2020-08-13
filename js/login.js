25bdc7352591723510984a5bcf06f1310e2ad1f0
RICARDO MASSARU OOSSAWA <005886569@itaud.des.ihf>
2020-08-13 18:48:16 -0300

    massaru

--- js/login.js
+++ js/login.js
@@ -1,30 +1,41 @@
 function logar(){
- 
-    let  login =  document.getElementById("txtLogin").value;
-    let senha =  document.getElementById("txtSenha").value;
+    var mensagem = {
+        login: document.getElementById("txtLogin").value,
+        senha: document.getElementById("txtSenha").value
+     }
+  
+    verificaLogin(mensagem);
 
-    let gerente = {
-        nome : login,
-        senha : senha
+   var cabecalho = {
+       method:"POST",
+       body:JSON.stringify(mensagem),
+       header:{
+           "Content-Type":"application/json"
+       }
     }
 
-      
-    let config = {
-        method: 'POST',
-        body: JSON.stringify(gerente),
-        headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-    }}
-
-    
-    fetch('http://localhost:8080/login', config)
-    .then(resp => resp.json())
-    .then(resp => {
-        console.log(resp);
-        alert('entrou');
-    }).catch(error =>{
-        alert('Usuairo ou senha incorreta');
+    fetch("http://localhost:8080/login", cabecalho)
+    .then(res => res.json())
+    .then(res=>{
+        localStorage.setItem("logado",JSON.stringify(res));
+        window.location="usuario.html";
     })
+    .catch(err=>{
+        window.alert("Erro");
+    });
+}
 
+function verificaLogin(mensagem){
+    if (mensagem.login.contains('@'))
+    {
+        console.log('e um e-mail');
+    }
+    else  if (mensagem.login.lenght() == 9)
+        {
+        console.log('e uma funcional');
+        }
+    else (mensagem.login.lenght() == 7)
+        {
+        console.log('e uma racf');
+        }
 }
