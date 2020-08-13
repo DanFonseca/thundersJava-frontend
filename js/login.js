1d3e75270e487824b4c31dfc818f1e2cddebf864
danie <daniel.freitas.ms1@gmail.com>
2020-08-13 15:56:32 -0300

    login

--- js/login.js
+++ js/login.js
@@ -1,36 +1,30 @@
 function logar(){
-    var mensagem = {
-        login: document.getElementById("txtLogin").value,
-        senha: document.getElementById("txtSenha").value
-     }
-  
-    verificaLogin (mensagem);
+ 
+    let  login =  document.getElementById("txtLogin").value;
+    let senha =  document.getElementById("txtSenha").value;
 
-   var cabecalho = {
-       method:"POST",
-       body:JSON.stringify(mensagem),
-       header:{
-           "Content-Type":"application/json"
-       }
+    let gerente = {
+        nome : login,
+        senha : senha
     }
 
-    fetch("http://localhost:8080/login", cabecalho)
-    .then(res => res.json())
-    .then(res=>{
-        localStorage.setItem("logado",JSON.stringify(res));
-        window.location="usuario.html";
+      
+    let config = {
+        method: 'POST',
+        body: JSON.stringify(gerente),
+        headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+    }}
+
+    
+    fetch('http://localhost:8080/login', config)
+    .then(resp => resp.json())
+    .then(resp => {
+        console.log(resp);
+        alert('entrou');
+    }).catch(error =>{
+        alert('Usuairo ou senha incorreta');
     })
-    .catch(err=>{
-        window.alert("Erro");
-    });
-}
 
-function verificaLogin (mensagem){
-    if (mensagem.login.contains('@')){
-        console.log('e um e-mail');
-    }else  if (mensagem.login.lenght() == 9){
-        console.log('e uma funcional');
-    }else (mensagem.login.lenght() == 7){
-        console.log('e uma racf');        
-    }
 }
