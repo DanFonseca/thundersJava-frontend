7f67b700e18fd900cd7375d6bc7cc637e15b3c09
unknown <adriana_snts86@outlook.com>
2020-08-14 02:51:02 -0300

    validacoes e ajuste do formulario de agendamento

--- js/agendamento.js
+++ js/agendamento.js
@@ -1,82 +1,92 @@
-function agendar(){
+function agendar() {
 
-        //Converção na formatação de data.
-        var Data = document.getElementById("txtData").value;
-        var ano = Data.substring(0,4);
-        var mes = Data.substring(5,7);
-        var dia = Data.substring(8);
-        var dataFormatada = dia + "/" + mes + "/" + ano;
+
+    // somar 1 dia a data atual
+    var hoje = new Date();
+    var ano1 = hoje.getFullYear();
+    var mes1 = hoje.getMonth() + 1;
+    var dia1 = hoje.getDate() + 1;
+    var dataAgenda = dia1 + "/" + mes1 + "/" + ano1;
+
+    var dmy = dataAgenda.split("/");
+    var dtamanha = new Date(dmy[2], dmy[1] - 1, dmy[0]);
+
+    //Converção na formatação de data.
+    var Data = document.getElementById("txtData").value;
+    var ano = Data.substring(0, 4);
+    var mes = Data.substring(5, 7);
+    var dia = Data.substring(8);
+    var dataFormatada = dia + "/" + mes + "/" + ano;
+
+    var dmy = dataFormatada.split("/");
+    var dtInformada = new Date(dmy[2], dmy[1] - 1, dmy[0]);
+
+    //Validar data 
+
+    if (Data == "") {
+        return alert("O campo data é obrigatório");
+    } else if (dtInformada < dtamanha) {
+        return alert("A data não pode ser igual ou anterior a data de hoje");
+    }
+
+    //Validar hora
+    var Hora = document.getElementById("txtHora").value;
+    var horaInformada = parseInt(Hora.substring(0, 2));
+    if (Hora == "") {
+        return alert("O campo hora é obrigatório");
+    } else if (horaInformada < 10 || horaInformada > 14) {
+        return alert("O horário de atendimento é das 10:00-AM às 14:00-PM");
+    }
 
     var mensagem = {
-        nome_cli : document.getElementById("txtNome").value,
-        email_cli: document.getElementById("txtEmail").value,
-        celular_cli : document.getElementById("txtCelular").value,
-        data : dataFormatada,
-        hora : document.getElementById("txtHora").value,
-        observacao : document.getElementById("txtObservacao").value,
-        agencia:{id:document.getElementById("cmbAgencias").value }        
+        nomeCliente: document.getElementById("txtNome").value,
+        emailCliente: document.getElementById("txtEmail").value,
+        celularCliente: document.getElementById("txtCelular").value,
+        data: dataFormatada,
+        hora: Hora,
+        observacao: document.getElementById("txtObservacao").value,
+        agencia: { id: document.getElementById("cmbAgencias").value }
     }
-    window.alert(JSON.stringify(mensagem));  
-    window.alert("passei aqui");  
+
+
     var cabecalho = {
-        method:"POST",
-        body:JSON.stringify(mensagem),
-        headers:{
+        method: "POST",
+        body: JSON.stringify(mensagem),
+        headers: {
             "Accept": "application/json",
-            "Content-Type":"application/json"
+            "Content-Type": "application/json"
         }
     }
-    window.alert(cabecalho);  
+    window.alert(cabecalho);
 
     fetch("http://localhost:8080/agendar", cabecalho)
-    .then(res => res.json())
-    .then(res => {
+        .then(res => res.json())
+        .then(res => {
             window.alert("Gravado com sucesso");
         })
-    .catch(err => 
-        {
+        .catch(err => {
             window.alert("Erro");
         });
-    }
+}
 
 // função para listar todas as agências cadastradas usando o método GET
 
-function carregarAgencias(){
+function carregarAgencias() {
 
-    // somar 1 dia a data atual
-        var hoje = new Date();  
-        var ano1 = hoje.getFullYear();
-        var mes1 = hoje.getMonth();
-        var dia1 = hoje.getDate()+1;
-        var dataAgenda = ano1.concat(mes1, dia1);
-
-        var Dt = 20200814
-        if (Data <= dataAgenda){
-            window.alert("Data menor ou igual a data atual");
-        }
-
-
-
-       
-       
-
-    //dt.setDate(dt.getDate() + 1);
-
-    window.alert(dataAgenda);
 
     fetch("http://localhost:8080/agencias")
-    .then(res=>res.json())
-    .then(res => preencherAgencias(res));
+        .then(res => res.json())
+        .then(res => preencherAgencias(res));
 
-}    
+}
 
-function preencherAgencias(lista){
+function preencherAgencias(lista) {
 
     var saida = "";
 
-    for (i=0; i<lista.length;i++){
-        saida+=
-          "<option value =  ' " + lista[i].id + " '>" + lista[i].nomeAgencia  + "</option>";
+    for (i = 0; i < lista.length; i++) {
+        saida +=
+            "<option value =  ' " + lista[i].id + " '>" + lista[i].nomeAgencia + "</option>";
     }
-    document.getElementById("cmbAgencias").innerHTML=saida;
+    document.getElementById("cmbAgencias").innerHTML = saida;
 }
